a6b8e5f5dc835656fa908275db5e8a95e0659f1d
suhash2106 <suhash2106@gmail.com>
2024-11-23 12:06:19 +0530

    added loops

--- variables_java/src/Arrays/Loop.java
+++ variables_java/src/Arrays/Loop.java
@@ -1,7 +1,8 @@
 package Arrays;
-
 public class Loop {
     public static void main(String[] args) {
-
+        for(int counter = 0;counter<40; counter = counter + 1)
+            System.out.println("Hello World");
     }
+
 }
