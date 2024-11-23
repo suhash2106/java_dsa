8e4fd02b7b55814891ceef3f5747cbebc84243e0
suhash2106 <suhash2106@gmail.com>
2024-11-23 12:48:28 +0530

    added loops

--- variables_java/src/Arrays/Loop.java
+++ variables_java/src/Arrays/Loop.java
@@ -1,8 +1,25 @@
 package Arrays;
+import java.util.*;
+
 public class Loop {
     public static void main(String[] args) {
-        for(int counter = 0;counter<100; counter = counter + 1)
-            System.out.println("Hello World");
-    }
+       // int i = 0;
+       // int j = 0;
+       // while(i<4){
+           // i = i + 1;
+            //j = j + i;
+            //System.out.println(j+" ");
+        //}
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int table = 0;
+        while(i<=10){
+            i = i + 1;
+            table = (n*i);
+            System.out.println(table);
+        }
 
+
+    }
 }
